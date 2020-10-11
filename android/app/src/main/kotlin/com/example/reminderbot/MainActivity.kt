c2dd6d91f532cbf4280da14f903c2df3b43ca4f5
Kamil <kamilanwar2001@gmail.com>
2020-10-11 12:59:17 +0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/reminderbot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reminderbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/item.dart
@@ -0,0 +1,9 @@
+class Item{
+  String itemName;
+  double price;
+  int quantity;
+
+  Item(this.itemName,this.price,this.quantity);
+
+
+}
